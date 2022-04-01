d27a21de380a1dd19ff0d02aa417e9d71edee6ad
Julius Simutis <julsimass@gmail.com>
2022-04-01 16:03:21 +0300

    Removing junk

---
+++ src/components/App/index.jsx
@@ -0,0 +1,14 @@
+import Description from "./components/Description";
+
+function App() {
+  return (
+    <div className="app">
+      {/* Header to change language */}
+
+      {/* Description about cats */}
+      <Description />
+    </div>
+  );
+}
+
+export default App;
